231afe45586416cb9c83bf1d5414855664c38530
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-08-27 11:40:18 +0600

    route add

--- components/Navber.jsx
+++ components/Navber.jsx
@@ -42,42 +42,22 @@ export default function Navber() {
     <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
       <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a
-            href="#"
-            className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-            aria-current="page"
-          >
-            Home
-          </a>
+         <Link href={'/'}>Home</Link>
         </li>
         <li>
           <Link href={'/about'}>About</Link>
+        </li>
+        <li>
+          <Link href={'/services'}>Services</Link>
             
-         
         </li>
         <li>
-          <a
-            href="#"
-            className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-          >
-            Services
-          </a>
+          <Link href={'/pricing'}>Pricing</Link>
+            
         </li>
         <li>
-          <a
-            href="#"
-            className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-          >
-            Pricing
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-          >
-            Contact
-          </a>
+            <Link href={'/contact'}>Contact</Link>
+            
         </li>
       </ul>
     </div>
