42737db4cc36e6e88897e4c87fe69e72877e36ce
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-09-07 10:51:59 +0600

    image and fonts add

--- app/page.js
+++ app/page.js
@@ -1,7 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
+import { Poppins } from 'next/font/google'
+
+import img from '../public/hello.jpg'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500','600','700']
+})
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <h1 className={`text-blue-600 font-bold text-5xl ${poppins.className}`}>This is Home</h1>
+      <Image src={img} width={500} height={400}/>
+    </div>
   )
 }
